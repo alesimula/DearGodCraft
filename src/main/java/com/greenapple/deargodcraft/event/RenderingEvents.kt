751db3d3996332af689b87ea9f54c856ebf069a7
Alex Sim <ale.simula97@gmail.com>
2020-05-24 18:00:43 +0200

    Randomly turn into any entity, random spawns

--- src/main/java/com/greenapple/deargodcraft/event/RenderingEvents.kt
+++ src/main/java/com/greenapple/deargodcraft/event/RenderingEvents.kt
@@ -5,14 +5,23 @@ import com.greenapple.deargodcraft.delegate.reflectField
 import com.greenapple.deargodcraft.timedRandom
 import com.greenapple.deargodcraft.utils.morph
 import com.greenapple.deargodcraft.utils.registry
+import com.mojang.blaze3d.matrix.MatrixStack
+import com.mojang.blaze3d.vertex.IVertexBuilder
 import net.minecraft.advancements.Advancement
 import net.minecraft.advancements.AdvancementRewards
 import net.minecraft.client.Minecraft
 import net.minecraft.client.entity.player.AbstractClientPlayerEntity
 import net.minecraft.client.gui.advancements.AdvancementsScreen
 import net.minecraft.client.renderer.entity.BipedRenderer
+import net.minecraft.client.renderer.entity.LivingRenderer
+import net.minecraft.client.renderer.entity.MobRenderer
 import net.minecraft.client.renderer.entity.PlayerRenderer
+import net.minecraft.client.renderer.entity.model.BipedModel
+import net.minecraft.client.renderer.entity.model.EntityModel
+import net.minecraft.client.renderer.entity.model.PlayerModel
+import net.minecraft.client.renderer.model.ModelRenderer
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.MobEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.util.ResourceLocation
@@ -24,6 +33,12 @@ import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.client.registry.IRenderFactory
 import net.minecraftforge.fml.client.registry.RenderingRegistry
 import net.minecraftforge.registries.GameData
+import java.lang.reflect.Field
+
+private val playerModelRenderers: List<Field> by lazy {PlayerModel::class.java.declaredFields.filter {property -> property.isAccessible = true; ModelRenderer::class.java.isAssignableFrom(property.type)}}
+private val bipedModelRenderers: List<Field> by lazy {BipedModel::class.java.declaredFields.filter {property -> property.isAccessible = true; ModelRenderer::class.java.isAssignableFrom(property.type)}}
+private fun PlayerModel<*>.hidePlayerRenderers() = playerModelRenderers.forEach {(it.get(this) as ModelRenderer).showModel = false}
+private fun BipedModel<*>.hideBipedRenderers() = bipedModelRenderers.forEach {(it.get(this) as ModelRenderer).showModel = false}
 
 class RenderingEvents {
     //private val MODEL_SNOWMAN = ModelPlayerSnowMan(0)
@@ -32,18 +47,37 @@ class RenderingEvents {
     private val RenderingRegistry.entityRenderersKt: MutableMap<EntityType<*>, IRenderFactory<*>> by reflectField("entityRenderers")
     private val RenderingRegistry?.instanceKt: RenderingRegistry by reflectField("INSTANCE")
 
-    private val World.bipedTextures by lazy {
+    private val World.bipedTextures: MutableList<Pair<PlayerModel<*>, ResourceLocation>> by lazy {
         val minecraft = Minecraft.getInstance()
         val world by lazy {Minecraft.getInstance().world as World}
         val renderers = minecraft.renderManager.renderers
-        EntityType::class.registry.values.mapNotNullTo(mutableListOf()) { type->
-            (renderers[type] as? BipedRenderer<MobEntity, *>)?.run {runCatching {getEntityTexture(null)}.getOrElse {
-                (type.create(world) as? MobEntity)?.let { tempEntity ->
+        WorldEvents.creatures.mapNotNullTo(mutableListOf()) { type->
+            (renderers[type] as? MobRenderer<MobEntity, *>)?.run {runCatching {null!!}.getOrElse {
+                (type.create(world) as? MobEntity)?.let {tempEntity ->
                     val texture = getEntityTexture(tempEntity)
                     tempEntity.remove()
-                    texture
+                    Pair(WrappingPlayerModel(entityModel, tempEntity), texture)
                 }
             }}
+        }
+    }
+
+    class WrappingPlayerModel<E: LivingEntity>(val wrappedModel: EntityModel<E>, val mockEntity: E): PlayerModel<E>(0F, false) {
+        init {
+            hidePlayerRenderers()
+            hideBipedRenderers()
+            /*if (wrappedModel is BipedModel<*>) wrappedModel.hideBipedRenderers()
+            else hideBipedRenderers()*/
+        }
+
+        override fun render(matrixStackIn: MatrixStack, bufferIn: IVertexBuilder, packedLightIn: Int, packedOverlayIn: Int, red: Float, green: Float, blue: Float, alpha: Float) {
+            wrappedModel.render(matrixStackIn, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha)
+        }
+        override fun setLivingAnimations(entity: E, limbSwing: Float, limbSwingAmount: Float, partialTick: Float) {
+            wrappedModel.setLivingAnimations(mockEntity, limbSwing, limbSwingAmount, partialTick)
+        }
+        override fun setRotationAngles(entity: E, limbSwing: Float, limbSwingAmount: Float, ageInTicks: Float, netHeadYaw: Float, headPitch: Float) {
+            wrappedModel.setRotationAngles(mockEntity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch)
         }
     }
 
@@ -51,7 +85,9 @@ class RenderingEvents {
 
     private fun PlayerRenderer.onRenderPlayer(player: PlayerEntity) {
         val clientPlayer = player as? AbstractClientPlayerEntity
-        morph(player, null, player.world.bipedTextures.apply {if (!playerTextureLoaded && clientPlayer!=null) {playerTextureLoaded = true; add(clientPlayer.locationSkin)}}.timedRandom)
+        val newModelPairList = player.world.bipedTextures.apply {if (!playerTextureLoaded && clientPlayer!=null) {playerTextureLoaded = true; add(Pair(this@onRenderPlayer.entityModel, clientPlayer.locationSkin))}}
+        val random = newModelPairList.timedRandom
+        morph(player, random?.first as PlayerModel<AbstractClientPlayerEntity>?, random?.second)
         /*if (!player.isInWater) player.getActivePotionEffect(DearGodCraft.Effects.MORPH_SNOWMAN)?.also { effect ->
             effect.durationKt = 72011
             morph(player, MODEL_SNOWMAN, TEXTURE_SNOWMAN)
--- src/main/java/com/greenapple/deargodcraft/event/WorldEvents.kt
+++ src/main/java/com/greenapple/deargodcraft/event/WorldEvents.kt
@@ -1,6 +1,21 @@
 package com.greenapple.deargodcraft.event
 
+import com.greenapple.deargodcraft.delegate.reflectConstructor
+import com.greenapple.deargodcraft.utils.registry
+import net.minecraft.client.Minecraft
+import net.minecraft.entity.*
+import net.minecraft.util.math.BlockPos
+import net.minecraftforge.event.entity.living.LivingSpawnEvent
+import net.minecraftforge.eventbus.api.Event
+import net.minecraftforge.eventbus.api.SubscribeEvent
+import kotlin.random.Random
+
 class WorldEvents {
+    companion object {
+        val creatures by lazy {arrayOf(EntityClassification.MONSTER, EntityClassification.CREATURE, EntityClassification.WATER_CREATURE).let {creatureClassifications ->
+            EntityType::class.registry.values.filter {it.classification in creatureClassifications}
+        }}
+    }
     /*@SubscribeEvent
     fun onWorldLoad(event: WorldEvent.Load) = event.runClient {
         if (world.dimension.type.id == DearGodCraft.DIMENSION.type.id) {
@@ -16,4 +31,24 @@ class WorldEvents {
             Blocks.WALL_TORCH.overrideLightValue()
         }
     }*/
+
+    tailrec fun List<EntityType<*>>.weightedRandom(): EntityType<*> = when(val type = random()) {
+        EntityType.WITHER -> if (Random.nextInt(0, 100) == 1) type else weightedRandom()
+        EntityType.ENDERMAN -> if (Random.nextInt(0, 100) == 1) type else weightedRandom()
+        EntityType.ELDER_GUARDIAN -> if (Random.nextInt(0, 100) == 1) type else weightedRandom()
+        else -> type
+    }
+
+    private val newSpawnReason by reflectConstructor<SpawnReason>()
+    private val SPAWN_REASON_DEARGOD = newSpawnReason()
+
+    @SubscribeEvent
+    fun onEntitySpawn(event: LivingSpawnEvent.SpecialSpawn) = event.takeIf {event.entity is MobEntity && event.spawnReason !== SPAWN_REASON_DEARGOD}?.run {
+        val world = kotlin.runCatching {Minecraft.getInstance().world}.getOrNull()
+        if (world != null) runCatching {
+            creatures.weightedRandom().spawn(event.entity.world, null, null, BlockPos(event.x, event.y, event.z), SPAWN_REASON_DEARGOD, true, false)
+        }
+        isCanceled = true
+        result = Event.Result.DENY
+    }
 }
